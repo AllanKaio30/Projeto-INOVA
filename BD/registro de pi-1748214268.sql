dc25d6baff0b02bddd4048a9d462875b1cc2b67e
thalysong <126722497+thalysong@users.noreply.github.com>
2025-06-13 19:21:39 -0300

    Update registro de pi-1748214268.sql

--- BD/registro de pi-1748214268.sql
+++ BD/registro de pi-1748214268.sql
@@ -1,21 +1,21 @@
 CREATE TABLE IF NOT EXISTS "usuarios" (
 	"id" serial NOT NULL UNIQUE,
-	"nome" varchar(255) NOT NULL,
-	"telefone" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"genero" varchar(255) NOT NULL,
+	"nome" varchar(100) NOT NULL,
+	"telefone" varchar(15) NOT NULL,
+	"email" varchar(100) NOT NULL,
+	"genero" varchar(20) NOT NULL,
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "pi" (
 	"id" serial NOT NULL UNIQUE,
 	"tipo_de_pi/patente" varchar(255) NOT NULL,
-	"titulo" varchar(255) NOT NULL UNIQUE,
-	"protocolo" varchar(255) NOT NULL,
+	"titulo" varchar(100) NOT NULL UNIQUE,
+	"protocolo" varchar(20) NOT NULL,
 	"termo_de_cessão" boolean NOT NULL,
-	"depositante" varchar(255) NOT NULL,
-	"status" varchar(255) NOT NULL,
-	"nome_do_parceiro" varchar(255),
+	"depositante" varchar(100) NOT NULL,
+	"status" varchar(30) NOT NULL,
+	"nome_do_parceiro" varchar(100),
 	"data_de_entrada" date,
 	PRIMARY KEY ("id")
 );
@@ -23,11 +23,11 @@ CREATE TABLE IF NOT EXISTS "pi" (
 CREATE TABLE IF NOT EXISTS "pagamentos" (
 	"id" serial NOT NULL UNIQUE,
 	"pi_id" bigint NOT NULL,
-	"etapa_pagamento" varchar(255) NOT NULL,
+	"etapa_pagamento" varchar(30) NOT NULL,
 	"data_do_pagamento" date NOT NULL,
 	"valor" double precision NOT NULL,
-	"status_de_pagameto" varchar(255) NOT NULL,
-	"processo_sei" varchar(255),
+	"status_de_pagameto" varchar(30) NOT NULL,
+	"processo_sei" varchar(50),
 	PRIMARY KEY ("id")
 );
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS "usuarios_pi" (
 
 CREATE TABLE IF NOT EXISTS "titulares" (
 	"id" serial NOT NULL UNIQUE,
-	"nome" varchar(255) NOT NULL,
+	"nome" varchar(100) NOT NULL,
 	PRIMARY KEY ("id")
 );
 
@@ -50,11 +50,11 @@ CREATE TABLE IF NOT EXISTS "titulares_pi" (
 CREATE TABLE IF NOT EXISTS "controle_processos" (
 	"id" serial NOT NULL UNIQUE,
 	"pi_id" bigint NOT NULL,
-	"numero_processo" varchar(255) NOT NULL UNIQUE,
+	"numero_processo" varchar(50) NOT NULL UNIQUE,
 	"data_criacao" date NOT NULL,
-	"assunto" varchar(255) NOT NULL UNIQUE,
-	"situacao" varchar(255) NOT NULL,
-	"obs" varchar(255) NOT NULL
+	"assunto" varchar(30) NOT NULL UNIQUE,
+	"situacao" varchar(30) NOT NULL,
+	"obs" varchar(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "RPI" (
@@ -62,18 +62,18 @@ CREATE TABLE IF NOT EXISTS "RPI" (
 	"data" date NOT NULL,
 	"pi_id" bigint NOT NULL,
 	"codigo_evento" double precision NOT NULL,
-	"tipo_de_evento" varchar(255) NOT NULL,
-	"descricao_do_evento" varchar(255),
+	"tipo_de_evento" varchar(30) NOT NULL,
+	"descricao_do_evento" varchar(50),
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "indicadores_anuais" (
 	"id" serial NOT NULL UNIQUE,
 	"ano" date NOT NULL,
-	"tipo_pi" varchar(255) NOT NULL,
+	"tipo_pi" varchar(30) NOT NULL,
 	"quantidade_total" bigint NOT NULL,
-	"origem" varchar(255) NOT NULL,
-	"tipo_de_incador" varchar(255) NOT NULL,
+	"origem" varchar(30) NOT NULL,
+	"tipo_de_incador" varchar(30) NOT NULL,
 	PRIMARY KEY ("id")
 );
 
