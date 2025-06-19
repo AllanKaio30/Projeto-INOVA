c1d726561a0f80bad365f73da47894070c355089
Allysson Peixoto <95757193+allysson19@users.noreply.github.com>
2025-06-19 14:24:39 -0300

    ENVIO DA TELA DE AUTORES

---
+++ Frontend/script.js
@@ -0,0 +1,15 @@
+function openAuthorModal() {
+    document.getElementById('authorModal').style.display = 'flex';
+}
+
+function closeAuthorModal() {
+    document.getElementById('authorModal').style.display = 'none';
+}
+
+function openEditModal() {
+    document.getElementById('editModal').style.display = 'flex';
+}
+
+function closeEditModal() {
+    document.getElementById('editModal').style.display = 'none';
+}
