bb9124a66830e161d1ff16748bc2d7162d275112
sergio <61068282+sergiolivr@users.noreply.github.com>
2025-07-08 01:40:32 -0300

    atualização scriptbancodedados.sql

--- BD/scriptbancodedados.sql
+++ BD/scriptbancodedados.sql
@@ -1,33 +1,33 @@
 CREATE TABLE IF NOT EXISTS "usuarios" (
 	"id" serial NOT NULL UNIQUE,
-	"nome" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
+	"nome" varchar(50) NOT NULL,
+	"email" varchar(50) NOT NULL,
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "pi" (
 	"id" serial NOT NULL UNIQUE,
-	"tipo" varchar(255) NOT NULL,
-	"titulo" varchar(255) NOT NULL UNIQUE,
-	"protocolo" varchar(255) NOT NULL,
+	"tipo" varchar(50) NOT NULL,
+	"titulo" varchar(50) NOT NULL,
+	"protocolo" varchar(50) NOT NULL,
 	"termo_de_cessao" boolean NOT NULL,
-	"depositante" varchar(255) NOT NULL,
-	"status" varchar(255) NOT NULL,
-	"nome_do_parceiro" varchar(255),
+	"depositante" varchar(20) NOT NULL,
+	"status" varchar(50) NOT NULL,
+	"nome_do_parceiro" varchar(20),
 	"data_de_entrada" date,
-	"sei" varchar(255) NOT NULL,
+	"sei" varchar(50),
 	"data_de_registro" date NOT NULL,
-	"titular" varchar(255) NOT NULL,
-	"responsavel" varchar(255) NOT NULL,
-	"telefone" varchar(255) NOT NULL,
-	"endereco" varchar(255) NOT NULL,
+	"titular" varchar(20) NOT NULL,
+	"responsavel" varchar(20),
+	"telefone" varchar(30) NOT NULL,
+	"endereco" varchar(30) NOT NULL,
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "pagamentos" (
 	"id" serial NOT NULL UNIQUE,
 	"pi_id" bigint NOT NULL,
-	"tipo_de_pagamento" varchar(255) NOT NULL,
+	"tipo_de_pagamento" varchar(50) NOT NULL,
 	"data_de_vencimento" date NOT NULL,
 	"valor" double precision NOT NULL,
 	"observacao" varchar(255) NOT NULL,
@@ -37,11 +37,11 @@ CREATE TABLE IF NOT EXISTS "pagamentos" (
 CREATE TABLE IF NOT EXISTS "controle_processos" (
 	"id" serial NOT NULL UNIQUE,
 	"pi_id" bigint NOT NULL,
-	"numero_processo" varchar(255) NOT NULL UNIQUE,
+	"numero_processo" varchar(50) NOT NULL UNIQUE,
 	"data_criacao" date NOT NULL,
-	"assunto" varchar(255) NOT NULL UNIQUE,
-	"situacao" varchar(255) NOT NULL,
-	"obs" varchar(255) NOT NULL
+	"assunto" varchar(50) NOT NULL UNIQUE,
+	"situacao" varchar(50) NOT NULL,
+	"obs" varchar(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS "RPI" (
@@ -49,29 +49,29 @@ CREATE TABLE IF NOT EXISTS "RPI" (
 	"data" date NOT NULL,
 	"pi_id" bigint NOT NULL,
 	"codigo_evento" double precision NOT NULL,
-	"tipo_de_evento" varchar(255) NOT NULL,
-	"descricao_do_evento" varchar(255),
+	"tipo_de_evento" varchar(50) NOT NULL,
+	"descricao_do_evento" varchar(50),
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "indicadores_anuais" (
 	"id" serial NOT NULL UNIQUE,
 	"ano" date NOT NULL,
-	"tipo_pi" varchar(255) NOT NULL,
+	"tipo_pi" varchar(50) NOT NULL,
 	"quantidade_total" bigint NOT NULL,
-	"origem" varchar(255) NOT NULL,
-	"tipo_de_incador" varchar(255) NOT NULL,
+	"origem" varchar(50) NOT NULL,
+	"tipo_de_incador" varchar(50) NOT NULL,
 	PRIMARY KEY ("id")
 );
 
 CREATE TABLE IF NOT EXISTS "autor" (
 	"id" serial NOT NULL UNIQUE,
-	"nome" varchar(255) NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"vinculo" varchar(255) NOT NULL,
-	"departamento" varchar(255) NOT NULL,
-	"campus" varchar(255) NOT NULL,
-	"universidade" varchar(255) NOT NULL,
+	"nome" varchar(50) NOT NULL,
+	"email" varchar(50) NOT NULL,
+	"vinculo" varchar(30) NOT NULL,
+	"departamento" varchar(50) NOT NULL,
+	"campus" varchar(30) NOT NULL,
+	"universidade" varchar(50) NOT NULL,
 	PRIMARY KEY ("id")
 );
 
